7f899fb7c6ea1d756fb0f2e479520fb5b73b58fa
Pavithran2 <pavithrank02@gmail.com>
2024-01-22 16:50:07 +0530

    container done

--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -5,10 +5,10 @@ import { Link } from 'react-router-dom'
 
 const SideNav = () => {
   return (
-    <Grid style={{ height: '100vh', display: 'flex', flexDirection: 'column', backgroundColor: 'black', color: 'white'}}>
+    <Grid style={{ height: '100vh', display: 'flex', flexDirection: 'column', alignContent: 'center', justifyContent: 'center',  backgroundColor: 'black', color: 'white'}}>
      <Link to='/'>Home</Link>
      <Link to='/about'>About</Link>
-     <Link to='/contact'>Contact</Link>
+     <Link to='/contact'>Contact</Link>,
      <Link to='/project'>Project</Link>
     </Grid>
   )
